e0a035bc23d498a72c58ac2ca0a7f7d6cb38bec5
ivanlikhvatov <ivanlikhvatov@icloud.com>
2022-03-17 00:23:44 +0400

    Initial commit

---
+++ src/main/java/org/example/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package org.example.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/bootstrap/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/4.1.1/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
